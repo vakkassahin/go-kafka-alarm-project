47b6c4d35cd033fcd7182652c8570207088dab3b
vakkassahin <119359293+vakkassahin@users.noreply.github.com>
2023-11-28 09:36:29 +0300

    Update producer.go

--- producer.go
+++ producer.go
@@ -2,78 +2,79 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"math/rand"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/Shopify/sarama"
 )
 
-const (
-	kafkaBrokers = "localhost:9092"
-	topic        = "temperature_data"
-	alarmTopic   = "temperature_alarms"
-)
-
 func main() {
+	// Kafka broker'ına bağlan
+	brokers := []string{"localhost:9092"} // Kafka broker adresi
 	config := sarama.NewConfig()
-	config.Producer.RequiredAcks = sarama.WaitForAll
-	config.Producer.Compression = sarama.CompressionSnappy
-	config.Producer.Flush.Frequency = 500 * time.Millisecond
-
-	producer, err := sarama.NewSyncProducer([]string{kafkaBrokers}, config)
+	producer, err := sarama.NewSyncProducer(brokers, config)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Error creating Kafka producer: %v\n", err)
+		return
 	}
-	defer func() {
-		if err := producer.Close(); err != nil {
-			log.Fatal(err)
+	defer producer.Close()
+
+	// Kafka topic'i belirle
+	topic := "cpu_memory_metrics"
+
+	// Ctrl+C sinyalini dinle
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+
+	// Belirli aralıklarla sahte CPU ve bellek verileri üret
+	go produceMetrics(producer, topic)
+
+	// Kafka'dan mesajları tüket
+	consumer, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		fmt.Printf("Error creating Kafka consumer: %v\n", err)
+		return
+	}
+	defer consumer.Close()
+
+	partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		fmt.Printf("Error creating partition consumer: %v\n", err)
+		return
+	}
+	defer partitionConsumer.Close()
+
+	for {
+		select {
+		case msg := <-partitionConsumer.Messages():
+			// Mesajları işle
+			fmt.Printf("Received message: %s\n", msg.Value)
+		case <-sigterm:
+			// Uygulamayı kapat
+			fmt.Println("Terminating...")
+			return
 		}
-	}()
+	}
+}
 
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
+func produceMetrics(producer sarama.SyncProducer, topic string) {
+	for {
+		cpuUsage := 80 + randFloat(-5, 5) // Sahte CPU kullanımı
+		memoryUsage := 70 + randFloat(-3, 3) // Sahte bellek kullanımı
 
-	doneCh := make(chan struct{})
+		message := fmt.Sprintf("CPU Usage: %.2f%%, Memory Usage: %.2f%%", cpuUsage, memoryUsage)
+		// Kafka'ya mesajı gönder
+		producer.SendMessage(&sarama.ProducerMessage{
+			Topic: topic,
+			Value: sarama.StringEncoder(message),
+		})
 
-	go func() {
-		for {
-			select {
-			case <-time.After(1 * time.Second):
-				temperature := rand.Float64() * 100.0
-				fmt.Printf("Current temperature: %.2f\n", temperature)
+		time.Sleep(5 * time.Second) // 5 saniyede bir metrik üret
+	}
+}
 
-				// Send temperature data to Kafka
-				msg := &sarama.ProducerMessage{
-					Topic: topic,
-					Value: sarama.StringEncoder(fmt.Sprintf("%.2f", temperature)),
-				}
-				_, _, err := producer.SendMessage(msg)
-				if err != nil {
-					log.Printf("Failed to produce message: %s", err)
-				}
-
-				// Check for alarm condition
-				if temperature > 90.0 {
-					// Send alarm message to Kafka
-					alarmMsg := &sarama.ProducerMessage{
-						Topic: alarmTopic,
-						Value: sarama.StringEncoder(fmt.Sprintf("High temperature alarm! %.2f", temperature)),
-					}
-					_, _, err := producer.SendMessage(alarmMsg)
-					if err != nil {
-						log.Printf("Failed to produce alarm message: %s", err)
-					}
-				}
-			case <-doneCh:
-				return
-			}
-		}
-	}()
-
-	<-signals
-	close(doneCh)
-	fmt.Println("Exiting...")
+func randFloat(min, max float64) float64 {
+	return min + rand.Float64()*(max-min)
 }
